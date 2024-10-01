e54599ed371a991e9623c27e83289c79a18602e3
hundong2 <ehdgns104@gmail.com>
2024-10-01 16:42:43 +0900

    Add TreeMap Example

--- Example/Main.java
+++ Example/Main.java
@@ -3,5 +3,7 @@ package Example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        TreeMap treeMap = new TreeMap();
+        treeMap.add(1, "one");
     }
 }
